1dd6b08361d29b65572aaeadb4657cf8f3f8bf4c
Amirhos_esm <44949989+Amirhos-esm@users.noreply.github.com>
2025-12-30 13:38:19 +0330

    changing api

--- iter.go
+++
@@ -1,7 +0,0 @@
-package startable
-
-type startable interface {
-	IsStarted() bool
-	Start() error
-	Stop() error
-}
--- stratable.go
+++ stratable.go
@@ -7,13 +7,14 @@ import (
 	"time"
 )
 
-type PreStartCallback func()
+// PreStartCallback is a function that is called before starting.
+// If it returns false, the start process is aborted.
+type PreStartCallback func() bool
 type StartFunction func(ctx context.Context) any
 type PostStartCallback func(any)
 
 type Startable struct {
-	mtx       sync.Mutex
-	isStarted bool
+	mtx sync.Mutex
 
 	ctx    context.Context
 	cancel context.CancelFunc
@@ -22,12 +23,25 @@ type Startable struct {
 	start StartFunction
 	post  PostStartCallback
 
-	endSig chan struct{}
+	state StartableState
+
+	endSig   chan struct{}
+	startSig chan struct{}
 }
+
+type StartableState int
+
+const (
+	Idle     StartableState = iota
+	Starting                // pre + start not completed
+	Running                 // start function executing
+	Stopping
+	Stopped
+)
 
 var (
 	ErrAlreadyStarted = errors.New("already started")
-	ErrNothingToStop  = errors.New("nothing to stop")
+	ErrNotStarted     = errors.New("not started")
 	// ErrStopTimeout is returned when StopWithTimeout exceeds the given duration
 	ErrStopTimeout = errors.New("stop timeout exceeded")
 )
@@ -36,7 +50,7 @@ var (
 func (s *Startable) SetPreStartCallback(cb PreStartCallback) {
 	s.mtx.Lock()
 	defer s.mtx.Unlock()
-	if s.isStarted {
+	if !s.isIdle() {
 		return
 	}
 	s.pre = cb
@@ -46,7 +60,7 @@ func (s *Startable) SetPreStartCallback(cb PreStartCallback) {
 func (s *Startable) SetStartFunction(fn StartFunction) {
 	s.mtx.Lock()
 	defer s.mtx.Unlock()
-	if s.isStarted {
+	if !s.isIdle() {
 		return
 	}
 	s.start = fn
@@ -56,52 +70,94 @@ func (s *Startable) SetStartFunction(fn StartFunction) {
 func (s *Startable) SetPostStartCallback(cb PostStartCallback) {
 	s.mtx.Lock()
 	defer s.mtx.Unlock()
-	if s.isStarted {
+	if !s.isIdle() {
 		return
 	}
 	s.post = cb
 }
 
-// IsStarted reports whether the Startable is running.
-func (s *Startable) IsStarted() bool {
+// IsActive reports whether the Startable is running sequence or not.
+func (s *Startable) IsActive() bool {
 	s.mtx.Lock()
 	defer s.mtx.Unlock()
-	return s.isStarted
+	return !s.isIdle()
+}
+
+// IsRunning reports whether the Startable is on Running state or not.
+func (s *Startable) IsRunning() bool {
+	s.mtx.Lock()
+	defer s.mtx.Unlock()
+	return s.state == Running
+}
+
+func (s *Startable) State() StartableState {
+	s.mtx.Lock()
+	defer s.mtx.Unlock()
+	return s.state
+}
+
+func (s *Startable) isIdle() bool {
+	return s.state == Idle || s.state == Stopped
 }
 
 // Start initializes and runs the Startable.
-func (s *Startable) Start() error {
+// if ctx is nil it will use background context
+func (s *Startable) Start(ctx context.Context) error {
 	s.mtx.Lock()
-	if s.isStarted {
+	if !s.isIdle() {
 		s.mtx.Unlock()
 		return ErrAlreadyStarted
 	}
 
-	s.ctx, s.cancel = context.WithCancel(context.Background())
+	if ctx == nil {
+		ctx = context.Background()
+	}
+	s.ctx, s.cancel = context.WithCancel(ctx)
 	s.endSig = make(chan struct{})
-	s.isStarted = true
+	s.startSig = make(chan struct{})
+	s.state = Starting
+
+	pre := s.pre
+	post := s.post
+	start := s.start
+
+	var out any
+
 	s.mtx.Unlock()
 
 	go func() {
 		// Run pre-start
-		if s.pre != nil {
-			s.pre()
+		if pre != nil {
+			if pre() == false {
+				close(s.startSig)
+				goto end
+			}
 		}
 
+		s.mtx.Lock()
+		s.state = Running
+		s.mtx.Unlock()
+
+		close(s.startSig)
 		// Run main start function
-		var out any
-		if s.start != nil {
-			out = s.start(s.ctx)
+
+		if start != nil {
+			out = start(s.ctx)
 		}
+
+		s.mtx.Lock()
+		s.state = Stopping
+		s.mtx.Unlock()
 
 		// Run post-start
-		if s.post != nil {
-			s.post(out)
+		if post != nil {
+			post(out)
 		}
 
+	end:
 		// Mark as stopped
 		s.mtx.Lock()
-		s.isStarted = false
+		s.state = Stopped
 		close(s.endSig)
 		s.mtx.Unlock()
 	}()
@@ -109,12 +165,39 @@ func (s *Startable) Start() error {
 	return nil
 }
 
+// WaitForFullStart blocks until the Startable has fully started.
+// It returns true if the Startable started successfully, or false if it stopped before starting.
+// if the Startable is not running, it returns false immediately.
+// if the Startable is already started, it returns true immediately.
+// if duration is non-zero, it will wait for that duration only.
+func (s *Startable) WaitForFullStart(duration time.Duration) bool {
+	s.mtx.Lock()
+	if s.isIdle() {
+		s.mtx.Unlock()
+		return false
+	}
+	s.mtx.Unlock()
+
+	if duration == 0 {
+		<-s.startSig
+		return s.IsRunning()
+
+	}
+	select {
+	case <-s.startSig:
+		// started
+		return s.IsRunning()
+	case <-time.After(duration):
+		return false
+	}
+}
+
 // Stop cancels the running Startable.
 func (s *Startable) Stop() error {
 	s.mtx.Lock()
-	if !s.isStarted {
+	if s.isIdle() {
 		s.mtx.Unlock()
-		return ErrNothingToStop
+		return ErrNotStarted
 	}
 	cancel := s.cancel
 	endSig := s.endSig
@@ -128,9 +211,9 @@ func (s *Startable) Stop() error {
 // StopWithTimeout attempts to stop the Startable, but returns an error if it doesn't stop within d.
 func (s *Startable) StopWithTimeout(d time.Duration) error {
 	s.mtx.Lock()
-	if !s.isStarted {
+	if s.isIdle() {
 		s.mtx.Unlock()
-		return ErrNothingToStop
+		return ErrNotStarted
 	}
 	cancel := s.cancel
 	endSig := s.endSig
